bd680ffb8ae3cd4336e7212c0cb4e0c95a13e113
Blueberryy <igory.ygr200@gmail.com>
2020-10-02 19:12:05 +0500

    Create beacon.lua

---
+++ lua/terrortown/lang/russian/beacon.lua
@@ -0,0 +1,17 @@
+local L = LANG.GetLanguageTableReference("Русский")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[BEACON.name] = "Маяк"
+L["info_popup_" .. BEACON.name] = [[Вы маяк! Маяк - это невиновный, который со временем может стать сильнее.
+
+Убив невиновного, вы потеряете свою роль.]]
+L["body_found_" .. BEACON.abbr] = "Он был маяком."
+L["search_role_" .. BEACON.abbr] = "Этот человек был маяком!"
+L["target_" .. BEACON.name] = "Маяк"
+L["ttt2_desc_" .. BEACON.name] = [[Вы маяк! Маяк - это невиновный, который со временем может стать сильнее.
+
+Убив невиновного, вы потеряете свою роль.]]
+
+-- OTHER ROLE LANGUAGE STRINGS
+L["buffs_received_" .. BEACON.name] = "Получено усилений: "
+L["max_power_" .. BEACON.name] = " (МАКС. МОЩЬ!)"
